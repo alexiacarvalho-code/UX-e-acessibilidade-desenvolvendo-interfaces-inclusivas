2f26b68824de340040d65c300885a81458edd8d3
alexiacarvalho-code <alexia.carvalho@escola.pr.gov.br>
2026-08-27 08:43:40 -0300

    aula3

---
+++ script.js
@@ -0,0 +1,15 @@
+let btnAjuda = document.querySelector(".inline-flex items-center gap-2 px-6 py-3 rounded-xl font-bold bg-red-600 text-white hover:bg-red-700 min-h-[52px] text-lg shadow-md");
+let btnFechar = document.querySelector (.modal-fecha)
+let modal = document.querySelector (.modal-fundo)
+
+let btnFechar = document.querySelector(".botao-fechar");
+let modal = document.querySelector(".modal-fundo");
+
+
+function abreModal() {
+    modal.style.display = "block";
+}
+
+function fechaModal() {
+    modal.style.display = "none";
+}
